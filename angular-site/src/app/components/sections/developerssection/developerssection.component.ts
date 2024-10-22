39945dffcd157e4e112070bb7248e66987827bf9
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-22 12:57:40 -0300

    feat: create section and add card component

---
+++ angular-site/src/app/components/sections/developerssection/developerssection.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { DevelopercardComponent } from './developercard/developercard.component';
+
+@Component({
+  selector: 'app-developerssection',
+  standalone: true,
+  imports: [CommonModule, DevelopercardComponent],
+  templateUrl: './developerssection.component.html',
+  styleUrl: './developerssection.component.css',
+})
+export class DeveloperssectionComponent {
+  developerList = [
+    {
+      name: 'Josefine',
+      img: 'assets/dev1.svg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Rudolf',
+      img: 'assets/dev2.svg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Lucy',
+      img: 'assets/dev3.svg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Robert',
+      img: 'assets/dev4.svg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+  ];
+}
